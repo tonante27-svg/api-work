3370a317ccbbc804923f5d1f60442e42e2776df2
TonyW <tonante27@gmail.com>
2026-09-16 14:25:30 -0400

    updated the mapComponents before using MapLibre to show the maps.

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -1,20 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class HelloConditionalRendering extends LightningElement {
-    isVisible = false;
-    name;
-    handleClick(event)
-    {
-        this.isVisible = true;
-    
-    }
-    changeHandler(event){
-        this.name = event.target.value;
-    }
-    get helloMethod(){
-        return this.name ==='hello';
-    }
-    // False values: x = 0, false, undefined, null, "" always return false
-
-
+  isVisible = false;
+  name;
+  handleClick() {
+    this.isVisible = true;
+  }
+  changeHandler(event) {
+    this.name = event.target.value;
+  }
+  get helloMethod() {
+    return this.name === "hello";
+  }
+  // False values: x = 0, false, undefined, null, "" always return false
 }
--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,62 +1,56 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class QuizApp extends LightningElement {
-    selectedanswer={};// for stroing answers
-    myQuestions = [{
-
-        id: "Question1",
-        question:"Which onbe of the following is not a template loop?",
-        answers:{
-            a:"for:each",
-            b:"iterator",
-            c:"map loop"
-        },
-        correctAnswer: "c"
+  selectedanswer = {}; // for stroing answers
+  myQuestions = [
+    {
+      id: "Question1",
+      question: "Which onbe of the following is not a template loop?",
+      answers: {
+        a: "for:each",
+        b: "iterator",
+        c: "map loop"
+      },
+      correctAnswer: "c"
     },
     {
-
-        id: "Question2",
-        question:"Which of the files is an invalid LWC component folder?",
-        answers:{
-            a:".svg",
-            b:".apex",
-            c:".js"
-        },
-        correctAnswer: "b"
-    },{
-
-        id: "Question3",
-        question:"Which of the following is not a directive?",
-        answers:{
-            a:"for:each",
-            b:"LWC:if",
-            c:"@track"
-        },
-        correctAnswer: "c"
+      id: "Question2",
+      question: "Which of the files is an invalid LWC component folder?",
+      answers: {
+        a: ".svg",
+        b: ".apex",
+        c: ".js"
+      },
+      correctAnswer: "b"
     },
-    ]
-    
-    // change Handler
-    changeHandler(event){
-        console.log(event.target.name)
-        console.log(event.target.value)
-       const{name,value} = event.target;
-       this.selectedanswer={...this.selectedanswer,[name]:value}
+    {
+      id: "Question3",
+      question: "Which of the following is not a directive?",
+      answers: {
+        a: "for:each",
+        b: "LWC:if",
+        c: "@track"
+      },
+      correctAnswer: "c"
     }
+  ];
 
-    submitHanlder(event){
+  // change Handler
+  changeHandler(event) {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    this.selectedanswer = { ...this.selectedanswer, [name]: value };
+  }
 
-    }
+  submitHanlder() {}
 
-    resetHandler(event){
+  get allNotSelected() {
+    // Keys = question id
+    // Values = question selected answer
+    // if all values are not selected return false
+    // if any value is selected return true
 
-
-        get allNotSelected(){
-            // Keys = question id
-            // Values = question selected answer
-            // if all values are not selected return false
-            // if any value is selected return true
-            return !(Object.keys(this.selectedanswer.length === this.myQuestions.legnth))
-        }
-    }
+    return Object.keys(this.selectedanswer).length !== this.myQuestions.length;
+  }
 }
